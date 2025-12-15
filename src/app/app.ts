6069f30e129d18c074f46338e98cf42186455537
shashi <shashiweb@gmail.com>
2025-12-15 22:53:25 +0530

    change

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,38 +1,18 @@
-import { CurrencyPipe, DatePipe, JsonPipe, NgFor, UpperCasePipe } from '@angular/common';
+
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { GeneralPipe } from './custompipes/general-pipe';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NgFor,UpperCasePipe,CurrencyPipe,DatePipe,JsonPipe,GeneralPipe],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('angularlatest');
 
-  name:string = "ravi singh";
-  age:number = 30;
-  emp:any[] = ["kumar",20,"mohan",true];
-  userlist:string[] = ["kumar","ravi","pankaj"];
-
-  info()
-  {
-    alert("welcome to msg");
-  }
-
-  worker:any[] = [{name:"ravi",age:20},{name:"rohit",age:30},{name:"pankaj",age:50,address:"delhi"}];
-
-
-employname:string = "mohan singh";
-price:number = 600;
-
-mydate:any = new Date();
-
-address:any = "delhi";
-
-
+  
 
 
 }
--- src/app/custompipes/general-pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { GeneralPipe } from './general-pipe';
-
-describe('GeneralPipe', () => {
-  it('create an instance', () => {
-    const pipe = new GeneralPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
