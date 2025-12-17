e9152d1c2c97b5f704387f4f35ceaa99e1582d10
shashi <shashiweb@gmail.com>
2025-12-17 23:38:31 +0530

    change

--- src/app/appmodules/auth/userlogin/userlogin.ts
+++ src/app/appmodules/auth/userlogin/userlogin.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import {NgStyle} from '@angular/common'
 
 @Component({
   selector: 'app-userlogin',
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './userlogin.html',
   styleUrl: './userlogin.scss',
 })
 export class Userlogin {
+
+fs:number = 70;
+
+xys()
+{
+  alert("welcome");
+}
+
+b:any;
+xys1(a:any)
+{
+  alert(a.target.value);
+  console.log(a);
+}
+
+mycolor:string ="red";
+xys2(a:any)
+{
+  this.mycolor = a.target.value;
+  console.log(a);
+}
 
 }
